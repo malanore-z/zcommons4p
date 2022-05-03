647d8272e212dab32f77b7414b57af350b6904ef
malanore-z <malanore.z@gmail.com>
2022-05-03 21:18:21 +0800

    [feat] Update required minimum python version to 3.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     package_dir={"": "src"},
     packages=["zcommons"],
     include_package_data=True,
